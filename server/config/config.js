2a331b884377ea572907569685f8f34bdbf5816f
Ilya Starikov <i.starikov@dunice.net>
2022-01-20 14:55:58 +0300

    fix config.js

--- server/config/config.js
+++ server/config/config.js
@@ -2,10 +2,10 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    username: "postgres",
+    username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: "news",
-    host: "127.0.0.1",
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
     dialect: "postgres"
   },
   test: {
